d613842c5805774fdc2765b3dee71775bcf2fa91
clakr <clarktolosa@gmail.com>
2024-03-23 12:45:36 +0800

    mobile and desktop

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,8 @@
+import days from "./data.json";
+
+const highest = Math.max(...days.map(({ amount }) => amount));
+const BASE = Math.ceil(highest / 10) * 10;
+
 function changeTextContent(element: HTMLTitleElement | Element | null) {
 	if (!element) return;
 
@@ -10,4 +15,17 @@ function changeTextContent(element: HTMLTitleElement | Element | null) {
 
 	const heading = document.querySelector(".sr-only");
 	changeTextContent(heading);
+
+	const chartSection = document.querySelector("section.chart");
+	if (!chartSection) return console.error("chartSection not found!");
+
+	const markup = days.reduce((previous, { day, amount }) => {
+		const height = Math.ceil((amount / BASE) * 100);
+
+		return `${previous}<article ${
+			highest === amount ? "data-highest" : ""
+		}><div style="height: ${height}%"><span>$${amount}</span></div><span>${day}</span></article>`;
+	}, "");
+
+	chartSection.insertAdjacentHTML("afterbegin", markup);
 })();
